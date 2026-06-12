1bdee4a61ad70fcabaa17c7a02ee8f7beb35678f
Gustavo Borges <borges@MacBook-Air-de-Gustavo.local>
2026-06-12 20:41:49 -0300

    feat: enable session record

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,5 +5,9 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
   ui_host: "https://us.posthog.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
+  disable_session_recording: false,
   debug: process.env.NODE_ENV === "development",
+  loaded: (ph) => {
+    ph.startSessionRecording(true);
+  },
 });
